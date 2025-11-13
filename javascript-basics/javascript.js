d1f4b5d3e5f024e73416254db292ce60829a2284
Big-Green117 <matthewafredrick@gmail.com>
2025-11-13 17:21:11 -0600

    initial javascript commit

---
+++ javascript-basics/javascript.js
@@ -0,0 +1,10 @@
+// Let keyword is used to declare variables
+let firstName = "John";
+
+// Const keyword is used to declare a constant
+const middleName = "Jimmy"
+
+// Deprecated keyword to declare variables, don't use but might come across
+var lastName = "Smith"
+
+console.log(firstName, middleName, lastName)
